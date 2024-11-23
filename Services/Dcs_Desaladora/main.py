8688637b70868f13956c9e51cd93d762a49186fd
Donkami04cd <camilo.munerac@gmail.com>
2024-11-23 01:00:00 -0500

    front-desaladora falta el home

--- Services/Dcs_Desaladora/main.py
+++ Services/Dcs_Desaladora/main.py
@@ -1,9 +1,16 @@
+import logger_config
+import requests
+import os
 import time
 import traceback
 import datetime
 import sched
 import logging
+import re
+import mysql.connector
 from dotenv import load_dotenv
+from config import database
+from db_connections import devnet_connection
 from db_get_data import get_data
 from api_cisco import get_cisco_id, get_cisco_data
 from api_prtg import get_prtg_id, get_prtg_data
@@ -16,7 +23,7 @@ def main():
     try:
 
         # Obtenemos los datos de los Clientes desde la BD
-        clients = get_data(table_name="candelaria_clients")
+        clients = get_data(table_name="desaladora_clients")
         if clients is None:
             raise ValueError(
                 "No se pudo establecer la conexión con la base de datos: el conector es None."
@@ -37,12 +44,16 @@ def main():
             clients_updated.append(client)
 
         devnet_bd_response = update_devnet_data(clients_updated)
-        historic_bd_response = save_historic_data(clients_updated)
+        # historic_bd_response = save_historic_data(clients_updated)
 
-        if devnet_bd_response is True and historic_bd_response is True:
-            datetime_register(system_name="candelaria_clients", status="OK")
+        if devnet_bd_response is True:
+            datetime_register(system_name="desaladora_clients", status="OK")
         else:
-            datetime_register(system_name="candelaria_clients", status="ERROR")
+            datetime_register(system_name="desaladora_clients", status="ERROR")
+        # if devnet_bd_response is True and historic_bd_response is True:
+        #     datetime_register(system_name="candelaria_clients", status="OK")
+        # else:
+        #     datetime_register(system_name="candelaria_clients", status="ERROR")
 
         logging.info("Ciclo finalizado con Exito!")
                     
